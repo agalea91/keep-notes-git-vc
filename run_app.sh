da9d5ed65e3366809bcb68a33b41a3a88837c99a
Alexander Galea <agalea91@gmail.com>
2020-05-24 19:44:27 -0700

    Update script and add ignores

--- run_app.sh
+++ run_app.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+cd /home/alex/keep-notes-git-vc
+
 echo "Updating local filestore"
 docker-compose up --build
 
-echo "Updated remote filestore"
-cd ~/Keep\ Notes\ Backup \
-   && git add notes/*.txt \
-   && git commit -m "Update notes" \
-   && git push origin master
+echo "Updating remote filestore"
+cd ~/Keep\ Notes\ Backup
+git pull remote origin
+git add notes/*.txt
+git commit -m "Update notes"
+git push origin master
 
